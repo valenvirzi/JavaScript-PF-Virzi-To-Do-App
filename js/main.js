de7083fe4f5c11a2a1d14f92f65903bfbbd7abae
Valentin Virzi <v.virzi@hotmail.com>
2023-08-10 17:16:39 -0300

    declaracion y armado de arrays

--- js/main.js
+++ js/main.js
@@ -15,16 +15,42 @@ const taskName = document.getElementById("taskName");
 const taskCategory = document.getElementById("taskCategory");
 const taskFinishTime = document.getElementById("taskFinishTime");
 const taskDescription = document.getElementById("taskDescription");
-const importantCheckbox = document.getElementById("importantCheckbox");
+const importanceCheckbox = document.getElementById("importanceCheckbox");
 const btnSubmitTask = document.getElementById("btnSubmitTask");
 const btnCloseForm = document.getElementById("btnCloseForm");
+const allTasksArray = [];
+let pendingTasksArray = [],
+  todayTasksArray = [],
+  importantTasksArray = [],
+  completedTasksArray = [];
+let taskId = 0;
+
+class Task {
+  constructor(
+    id,
+    name,
+    category,
+    finishTime,
+    description,
+    importance,
+    completed
+  ) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.finishTime = finishTime;
+    this.description = description;
+    this.importance = importance;
+    this.completed = completed;
+  }
+}
 
 function resetForm() {
   taskName.value = null;
   taskCategory.value = null;
   taskFinishTime.value = null;
   taskDescription.value = null;
-  importantCheckbox.checked = false;
+  importanceCheckbox.checked = false;
   taskFormDisplay.classList.add("d-none");
 }
 
@@ -32,15 +58,29 @@ function firstLetterCap(phrase) {
   return phrase.charAt(0).toUpperCase() + phrase.slice(1);
 }
 
-TODO:
-function formatDatetime() {}
+function formatDatetime(finishTimeTaskDate) {
+  return `${finishTimeTaskDate.getFullYear()}/${(
+    finishTimeTaskDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}/${finishTimeTaskDate
+    .getDate()
+    .toString()
+    .padStart(2, "0")} ${finishTimeTaskDate
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${finishTimeTaskDate
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}hs`;
+}
 
 function createTaskElement(
+  idTask,
   nameTask,
   categoryTask,
-  finishTimeTask,
-  descriptionTask,
-  importantTask
+  finishTimeTaskDate,
+  descriptionTask
 ) {
   nameTask = firstLetterCap(nameTask);
   categoryTask = firstLetterCap(categoryTask);
@@ -50,29 +90,57 @@ function createTaskElement(
   newTask.innerHTML = `
     <div class="card__color"></div>
     <div class="card__body">
-        <h5 class="card__category">${categoryTask}</h5>
-        <h4 class="card__name">${nameTask}</h4>
-        <div class="card__div">
-            <img class="card__svg" src="img/clock.svg" alt="clock">
-            <p class="card__p">
-                <span class="card__finish-time">${finishTimeTask}</span>
-            </p>
-        </div>
-        <div class="card__div">
-            <img class="card__svg desc-svg" src="img/paragraph.svg" alt="description">
-            <p class="card__p">
-                <span class="card__description">${descriptionTask}</span>
-            </p>
-        </div>
-        <button type="button" class="card__btn close">
-            <img class="card__btn-svg" src="img/close-x.svg" alt="close">
-        </button>
-        <button type="button" class="card__btn complete">
-            <img class="card__btn-svg" src="img/tick.svg" alt="complete">
-        </button>
+    <h5 class="card__category">${categoryTask}</h5>
+    <h4 class="card__name">${nameTask}</h4>
+    <div class="card__div">
+    <img class="card__svg" src="img/clock.svg" alt="clock">
+    <p class="card__p">
+    <span class="card__finish-time">${formatDatetime(finishTimeTaskDate)}</span>
+    </p>
+    </div>
+    <div class="card__div">
+    <img class="card__svg desc-svg" src="img/paragraph.svg" alt="description">
+    <p class="card__p">
+    <span class="card__description">${descriptionTask}</span>
+    </p>
+    </div>
+    <button type="button" class="card__btn close" id="btnDelete${idTask}">
+    <img class="card__btn-svg" src="img/close-x.svg" alt="close">
+    </button>
+    <button type="button" class="card__btn complete" id="btnComplete${idTask}">
+    <img class="card__btn-svg" src="img/tick.svg" alt="complete">
+    </button>
     </div>
     `;
   return newTask;
+}
+
+//TODO: Crear función que tome las Tareas de los diferentes Arrays y les haga Append en los respectivos contenedores según su Array
+
+//TODO: Encontrar la manera de que el botón de ELIMINAR de cada Tarea remueva la Tarea del Array principal y actualice TODOS los Arrays para dejar de mostrar la Tarea Eliminada
+
+//TODO: Encontrar la manera de que el botón de COMPLETAR de cada Tarea remueva la Tarea del Array de Pendientes, lo Añada al Array de Completadas & actualice TODOS los Arrays para mostrar la Tarea Completada en la sección de Tareas Completadas
+
+function appendTaskToContainer(taskContainer, task) {
+  taskContainer.appendChild(task);
+}
+
+function updateArrays() {
+  pendingTasksArray = allTasksArray.filter((task) => task.completed == false);
+  todayTasksArray = pendingTasksArray.filter((task) => {
+    const currentDate = new Date();
+    if (
+      currentDate.setHours(0, 0, 0, 0) == task.finishTime.setHours(0, 0, 0, 0)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  importantTasksArray = pendingTasksArray.filter(
+    (task) => task.importance == true
+  );
+  completedTasksArray = allTasksArray.filter((task) => task.completed == true);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -87,21 +155,59 @@ document.addEventListener("DOMContentLoaded", function () {
   // Función para añadir la tarea al documento
   taskForm.addEventListener("submit", function (e) {
     e.preventDefault();
+
     const nameTask = taskName.value;
     const categoryTask = taskCategory.value;
-    const finishTimeTask = taskFinishTime.value;
     const descriptionTask = taskDescription.value;
-    const importantTask = importantCheckbox.checked;
+    const importanceTask = importanceCheckbox.checked;
+    const finishTimeTask = taskFinishTime.value;
+    const finishTimeTaskDate = new Date(finishTimeTask);
 
-    todayTaskContainer.appendChild(
-      createTaskElement(
-        nameTask,
-        categoryTask,
-        finishTimeTask,
-        descriptionTask,
-        importantTask
-      )
+    let newTask = new Task(
+      taskId,
+      nameTask,
+      categoryTask,
+      finishTimeTaskDate,
+      descriptionTask,
+      importanceTask,
+      false
     );
+    allTasksArray.push(newTask);
+    updateArrays();
+    console.log("Todas");
+    console.log(allTasksArray);
+    console.log("Pendientes");
+    console.log(pendingTasksArray);
+    console.log("Hoy");
+    console.log(todayTasksArray);
+    console.log("Importantes");
+    console.log(importantTasksArray);
+    console.log("Completadas");
+    console.log(completedTasksArray);
+    // Aumento el valor de ID en 1 para diferenciar el siguiente objeto del objeto recién creado
+    taskId++;
+
+    // Mostrar Elemento
+    //
+    // todayTaskContainer.appendChild(
+    //   createTaskElement(
+    //     nameTask,
+    //     categoryTask,
+    //     finishTimeTask,
+    //     descriptionTask,
+    //     importanceTask
+    //   )
+    // );
     btnSubmitTask.addEventListener("pointerdown", resetForm());
   });
 });
+console.log("Todas");
+console.log(allTasksArray);
+console.log("Pendientes");
+console.log(pendingTasksArray);
+console.log("Hoy");
+console.log(todayTasksArray);
+console.log("Importantes");
+console.log(importantTasksArray);
+console.log("Completadas");
+console.log(completedTasksArray);
